8e45c4f9b7e46a1960bdbce50ece46757aced10f
github username <github email address>
2019-02-26 14:49:49 +0000

    Done.

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -4,7 +4,9 @@ const Sushi = (props) => {
   return (
     <div className="sushi">
       <div className="plate"> 
-          {props.url === '' ? null : <img src={props.url} width="100%" alt={props.price} id={props.id} onClick={props.handleSushiClick}/>}
+          {props.url === '' ? 
+                null :
+                 <img src={props.url} width="100%" alt={props.price} id={props.id} onClick={props.handleSushiClick}/>}
       </div>
       <h4 className="sushi-details">
         {props.name} - ${props.price}
