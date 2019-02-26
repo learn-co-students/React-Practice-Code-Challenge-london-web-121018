4a2940c379777739cdc41329120e9ce939fd3907
Claudia Fox <claudiafox95@icloud.com>
2019-02-26 10:20:20 +0000

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -4,14 +4,44 @@ import Table from './containers/Table';
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
+// const n = 0
 
 class App extends Component {
+
+  state = {
+    sushis: [],
+    currentFour: [0,4],
+    emptyPlates: [],
+    budget: 100
+  }
+
+
+  componentDidMount() {
+    return fetch(API)
+      .then(response => response.json())
+      .then(sushis => this.setState({sushis: sushis}))
+  }
+
+  nextFour = () => {
+    const nextFour = [this.state.currentFour[0] + 4, this.state.currentFour[1] + 4]
+    this.setState({currentFour: nextFour })
+  }
+
+  addEmptyPlate = price => {
+    const emptyPlates = this.state.emptyPlates.concat([0])
+    const budget = this.state.budget - price
+    this.setState({
+      emptyPlates: emptyPlates,
+      budget: budget
+    })
+  }
+
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushis={this.state.sushis} currentFour={this.state.currentFour} nextFour={this.nextFour} budget={this.state.budget} addEmptyPlate={this.addEmptyPlate}/>
+        <Table budget={this.state.budget} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,13 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
-const SushiContainer = (props) => {
+const SushiContainer = ({sushis, currentFour, nextFour, budget, addEmptyPlate}) => {
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {sushis.map(sushi => <Sushi key={sushi.id} sushi={sushi} budget={budget} addEmptyPlate={addEmptyPlate} />).slice(currentFour[0], currentFour[1])}
+        <MoreButton nextFour={nextFour} />
       </div>
     </Fragment>
   )
