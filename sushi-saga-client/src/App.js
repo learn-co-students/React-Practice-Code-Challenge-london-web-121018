9cb8479f8f6c6d0c7df401f344f79bb9017b0cb3
github username <github email address>
2019-02-26 09:33:06 +0000

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,17 +1,79 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
+import Form from './containers/Form';
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
+let sushiStart = 0;
+let sushiEnd = 4;
 
 class App extends Component {
+
+  state = {
+    sushis: ['Sushi is loading...'],
+    onBelt: [],
+    eatenSushi: [],
+    budget: 100
+  }
+
+  fetchSushi = () => {
+    fetch(API)
+      .then(resp => resp.json())
+      .then( data => {
+        this.setState({
+          sushis: data,
+          onBelt: data.slice(sushiStart, sushiEnd)
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.fetchSushi()
+  }
+
+  handleClick = () => {
+    sushiStart = sushiStart + 4;
+    sushiEnd = sushiEnd + 4;
+    console.log(sushiEnd, this.state.sushis.length)
+    if(this.state.sushis.length === sushiEnd) {
+      sushiStart = 0
+      sushiEnd = 4
+    } ;
+    this.setState({
+      onBelt: this.state.sushis.slice(sushiStart, sushiEnd)
+    })
+  }
+
+  handleBeingEaten = (id) => {
+    const sushi = this.state.sushis.find(sushi => sushi.id === id);
+    const subBudget = this.state.budget - sushi.price;
+
+    if(!this.state.eatenSushi.includes(id) && subBudget >= 0) {
+      this.setState({
+        eatenSushi: [...this.state.eatenSushi, id],
+        budget: subBudget
+      })
+    }
+  }
+
+  eaten = (id) => this.state.eatenSushi.includes(id) ? true : false
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    const amountAdded = parseInt(event.target.children[0].value);
+    this.setState({
+      budget: this.state.budget + amountAdded
+    })
+    event.target.reset();
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <Form handleFormSubmit={this.handleFormSubmit} />
+        <SushiContainer sushis={this.state.onBelt} handleClick={this.handleClick} handleBeingEaten={this.handleBeingEaten} eaten={this.eaten} />
+        <Table eatenSushi={this.state.eatenSushi} budget={this.state.budget} />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,15 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          props.sushis.map(sushi => <Sushi sushi={sushi} key={sushi.id} handleBeingEaten={props.handleBeingEaten} eaten={props.eaten} />)
         }
-        <MoreButton />
+        <MoreButton handleClick={props.handleClick} />
       </div>
     </Fragment>
   )
